b982e05c10b8cbe7a895d20fad2c371e043fea44
Harikrishna Ramesh <harikrishnaramesh777@gmail.com>
2024-11-18 16:35:10 +0530

    properties got added

--- myproject/myproject/spiders/productspider.py
+++ myproject/myproject/spiders/productspider.py
@@ -5,4 +5,23 @@ class ProductSpider(scrapy.Spider):
     name = 'product'
     start_urls = ['https://www.bayut.com/to-rent/property/dubai/']
 
-    
+    def parse(self, response):
+        for products in response.css('article.fbc619bc._058bd30f'):
+            yield{
+
+                property_id : "",
+                purpose : "",
+                type : "",
+                added_on : "22 April 2021",
+                furnishing: "Unfurnished",
+                price: {
+                    currency : "",
+                    amount: ""
+                },
+                location: "",
+                bed_bath_size : {
+                bedrooms : "" ,
+                bathrooms : "",
+                size : ""
+                },
+            }
